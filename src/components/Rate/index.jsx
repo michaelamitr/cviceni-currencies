9b4f83d5aa348515135b151e6bcf5815b44a5a08
Michaela Trčková <misa.trckova@volny.cz>
2023-05-20 16:41:26 +0200

    final

--- src/components/Rate/index.jsx
+++ src/components/Rate/index.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './style.css';
 
-const currencies = {
-  USD: { CZK: 23.823 },
-  EUR: { CZK: 24.74 },
-  GBP: { CZK: 29.067 }
-};
+// const currencies = {
+//   USD: { CZK: 23.823 },
+//   EUR: { CZK: 24.74 },
+//   GBP: { CZK: 29.067 },
+// };
 
-const Rate = () => {
+const Rate = ({ from }) => {
+  const [rate, setRate] = useState('XD');
+
+  useEffect(() => {
+    fetch(`https://api.frankfurter.app/latest?from=${from}&to=CZK`)
+      .then((response) => response.json())
+      .then((data) => setRate(data.rates.CZK));
+  }, [from]);
+
   return (
     <div className="rate">
-      <div className="rate__currency">1 USD</div>
+      <div className="rate__currency">1 {from}</div>
       <div>=</div>
-      <div className="rate__value">22 CZK</div>
+      <div className="rate__value">{rate} CZK</div>
     </div>
   );
 };
